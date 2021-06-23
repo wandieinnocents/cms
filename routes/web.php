86a4514d5ae69e2d4b75a8c45b1fcc05627e4fa4
WENAREEBA INNOCENT <wandieinnocent2@gmail.com>
2021-06-23 21:02:18 +0300

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// BACKEND ROUTES
+
+// dashboard
+Route::get('/dashboard' , 'App\Http\Controllers\AdminPagesController@dashboard');
+// orders
+Route::get('/orders' , 'App\Http\Controllers\AdminPagesController@view_orders');
+// quotes
+Route::get('/quotes' , 'App\Http\Controllers\AdminPagesController@quotes');
+
+
+
+// add product 
+Route::resource('/products','App\Http\Controllers\ProductController');
+// add product Category
+// Route::resource('/product_categories','App\Http\Controllers\ProductCategoryController');
+Route::resource('/categories','App\Http\Controllers\CategoryController');
+
+// Route::get('/add_product' , 'App\Http\Controllers\AdminPagesController@add_product');
+// view product 
+// Route::get('/view_product' , 'App\Http\Controllers\AdminPagesController@view_product');
+// image upload
+Route::resource('/imagess','App\Http\Controllers\ImageUploadController');
+
+
+
+
+
+
